7332e40c4018e9a863c4bd4bc57921b563e4a176
ansgar.hoyer <ansgar.hoyer@aoe.com>
2024-04-08 19:49:13 +0200

    chore: remove commited keys and remove unused dependencies

--- src/db/TableTennisDrizzleClient.ts
+++ src/db/TableTennisDrizzleClient.ts
@@ -6,15 +6,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 console.log("DOTENV HERE", process.env )
 export async function getConnectedDBClient() {
-    /**
-     *  database: "postgres",
-    host: "database-1.cluster-cv84auq62wfc.eu-central-1.rds.amazonaws.com",
-    password: "~EP3>q}ooC6#*l2(qt9aT)*1Z:wG",
-    user:"postgres"
-     host: process.env.DATABASE_HOST,
-            username: process.env.DATABASE_USERNAME,
-            password: process.env.DATABASE_PASSWORD,
-     */
+    
     const client = new Client({
         port: 5432,
         database: process.env.DATABASE,
@@ -22,13 +14,6 @@ export async function getConnectedDBClient() {
         password: process.env.DB_PASSWORD,
         user: process.env.DB_USER
     });
-
-    console.log(`DB Connection: ${{
-        database: process.env.DATABASE,
-        host: process.env.DB_HOST,
-        password: process.env.DB_PASSWORD,
-        user: process.env.DB_USER
-    }}`)
 
     await client.connect();
     const db = drizzle(client, { schema });
