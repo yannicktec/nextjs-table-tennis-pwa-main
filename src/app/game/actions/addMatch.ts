29a15f2e919150c0f813cc9b3b0ad7158aab1032
ansgar.hoyer <ansgar.hoyer@aoe.com>
2024-07-05 14:40:11 +0200

    feat: add logging for server actions

--- src/app/game/actions/addMatch.ts
+++ src/app/game/actions/addMatch.ts
@@ -1,54 +1,78 @@
-"use server"
-
+"use server";
+import * as Sentry from "@sentry/nextjs";
 import { getConnectedDBClient } from "@/db/TableTennisDrizzleClient";
 import * as schema from "@/db/schema";
 import { formDataToObject } from "@/lib/formDataToObject";
 import { revalidatePath } from "next/cache";
-import * as z from "zod"
+import * as z from "zod";
+import { headers } from "next/headers";
+
+export default async function withSentryAddMatch(formData: FormData) {
+  return await Sentry.withServerActionInstrumentation(
+    "new-player/actions/addPlayer", // The name you want to associate this Server Action with in Sentry
+    {
+      formData, // Optionally pass in the form data
+      headers: headers(), // Optionally pass in headers
+      recordResponse: true, // Optionally record the server action response
+    },
+    async () => addMatch(formData)
+  );
+}
 
 const inputSchema = z.object({
-    winnerId1: z.coerce.number({ required_error: "winnerId is required" }),
-    winnerId2: z.coerce.number({ required_error: "winnerId is required" }).optional(),
+  winnerId1: z.coerce.number({ required_error: "winnerId is required" }),
+  winnerId2: z.coerce
+    .number({ required_error: "winnerId is required" })
+    .optional(),
 });
-export default async function addMatch(formData: FormData) {
-    "use server"
+async function addMatch(formData: FormData) {
+  "use server";
 
-    try {
-        const { winnerId1, winnerId2 } = inputSchema.parse(formDataToObject(formData))
+  try {
+    const { winnerId1, winnerId2 } = inputSchema.parse(
+      formDataToObject(formData)
+    );
 
-        // Create a connection to the database
-        const db = await getConnectedDBClient()
-        console.log("got DB Connection")
+    // Create a connection to the database
+    const db = await getConnectedDBClient();
+    console.log("got DB Connection");
 
-        // Insert the match and the playerMatches as a transaction to ensure consistency
-        await db.transaction(async tx => {
-            const results = await tx.insert(schema.matches).values({
-                createdAt: new Date(),
-                enteredBy: 1,
-            }).returning({ id: schema.matches.id });
-            console.log("inserted new match:", results)
-            const matchId = results[0].id
-            const playermatchResult = await tx.insert(schema.playerMatches).values({
-                type: "WON",
-                match: matchId,
-                player: winnerId1,
-            }).execute();
-            console.log("inserted new playermatch:", playermatchResult)
-            if(winnerId2){
-                const playermatchResult2 = await tx.insert(schema.playerMatches).values({
-                    type: "WON",
-                    match: matchId,
-                    player: winnerId2,
-                }).execute();
-                console.log("inserted new playermatch:", playermatchResult2)
-            }
-            
+    // Insert the match and the playerMatches as a transaction to ensure consistency
+    await db.transaction(async (tx) => {
+      const results = await tx
+        .insert(schema.matches)
+        .values({
+          createdAt: new Date(),
+          enteredBy: 1,
         })
+        .returning({ id: schema.matches.id });
+      console.log("inserted new match:", results);
+      const matchId = results[0].id;
+      const playermatchResult = await tx
+        .insert(schema.playerMatches)
+        .values({
+          type: "WON",
+          match: matchId,
+          player: winnerId1,
+        })
+        .execute();
+      console.log("inserted new playermatch:", playermatchResult);
+      if (winnerId2) {
+        const playermatchResult2 = await tx
+          .insert(schema.playerMatches)
+          .values({
+            type: "WON",
+            match: matchId,
+            player: winnerId2,
+          })
+          .execute();
+        console.log("inserted new playermatch:", playermatchResult2);
+      }
+    });
 
-        revalidatePath("/game")
-
-    } catch (error) {
-        console.error(error);
-        throw new Error("Could not add match");
-    }
+    revalidatePath("/game");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Could not add match");
+  }
 }
--- src/app/new-player/actions/addPlayer.ts
+++ src/app/new-player/actions/addPlayer.ts
@@ -1,35 +1,54 @@
-"use server"
+"use server";
 
 import "dotenv/config";
+import * as Sentry from "@sentry/nextjs";
 
 import * as schema from "@/db/schema";
 import * as z from "zod";
 import { redirect } from "next/navigation";
 import { getConnectedDBClient } from "@/db/TableTennisDrizzleClient";
 import { formDataToObject } from "@/lib/formDataToObject";
+import { headers } from "next/headers";
+
+export default async function withSentryAddPlayer(formData: FormData) {
+  return await Sentry.withServerActionInstrumentation(
+    "new-player/actions/addPlayer", // The name you want to associate this Server Action with in Sentry
+    {
+      formData, // Optionally pass in the form data
+      headers: headers(), // Optionally pass in headers
+      recordResponse: true, // Optionally record the server action response
+    },
+    async () => addPlayer(formData)
+  );
+}
 
 const inputSchema = z.object({
-    name: z.string({ required_error: "Name is required" }),
-    emoji: z.string().emoji({ message: "Emoji is required" }),
+  name: z.string({ required_error: "Name is required" }),
+  emoji: z.string().emoji({ message: "Emoji is required" }),
 });
 
-export async function addPlayer(formData: FormData) {
-    const { name, emoji } = inputSchema.parse(formDataToObject(formData));
+async function addPlayer(formData: FormData) {
+  const { name, emoji } = inputSchema.parse(formDataToObject(formData));
 
-    const db = await getConnectedDBClient()
+  const db = await getConnectedDBClient();
 
-    const players = await db.query.players.findMany()
+  const players = await db.query.players.findMany();
 
-    if (players.some(player => player.name === name && player.emoji === emoji)) {
-        console.log("hier geht er net rein")
-    }
+  if (
+    players.some((player) => player.name === name && player.emoji === emoji)
+  ) {
+    console.log("hier geht er net rein");
+  }
 
-    await db.insert(schema.players).values({
-        name: name,
-        emoji: emoji,
-        createdAt: new Date(),
-        createdBy: 1,
-    }).execute();
+  await db
+    .insert(schema.players)
+    .values({
+      name: name,
+      emoji: emoji,
+      createdAt: new Date(),
+      createdBy: 1,
+    })
+    .execute();
 
-    redirect("/game")
+  redirect("/game");
 }
